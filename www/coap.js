e1c899832538c79679bcfa76ea7a414c3f2d6a1b
giacomo <giacomo.tanganelli@for.unipi.it>
2017-12-24 11:52:13 +0100

    js

--- www/coap.js
+++ www/coap.js
@@ -1,26 +1,87 @@
 /*global cordova, module*/
 
 var Coap = {
-    get: function (name, successCallback, errorCallback) {
+    get: function (request, successCallback, errorCallback) {
         if (typeof successCallback !== "function") {
-            console.log("get first parameter must be a function to handle responses.");
+            console.log("get second parameter must be a function to handle responses.");
             return;
         }
-        cordova.exec(successCallback, errorCallback, "Coap", "get", [name]);
+        var options = [];
+        if (request === null || typeof request !== "object") {
+            console.log("Coap: Invalid parameter. request not an object.");
+            return;
+        }
+        if (request.URI !== undefined && request.URI !== "") {
+            options.push(request.URI);
+        }
+        else {
+            console.log("Coap: No URI given.");
+            return false;
+        }
+        if (request.mediatype !== undefined && request.mediatype !== -1) {
+            options.push(request.mediatype);
+        }
+
+
+        cordova.exec(successCallback, errorCallback, "Coap", "get", options);
     },
-    post: function (name, options, successCallback, errorCallback) {
+    post: function (request, successCallback, errorCallback) {
         if (typeof successCallback !== "function") {
-            console.log("post first parameter must be a function to handle responses.");
+            console.log("post second parameter must be a function to handle responses.");
             return;
         }
-        cordova.exec(successCallback, errorCallback, "Coap", "post", [name, options]);
+        var options = [];
+        if (request === null || typeof request !== "object") {
+            console.log("Coap: Invalid parameter. request not an object.");
+            return;
+        }
+        if (request.URI !== undefined && request.URI !== "") {
+            options.push(request.URI);
+        }
+        else {
+            console.log("Coap: No URI given.");
+            return false;
+        }
+        if (request.payload !== undefined) {
+            options.push(request.payload);
+        }
+        else {
+            console.log("Coap: No payload given.");
+            return false;
+        }
+        if (request.mediatype !== undefined && request.mediatype !== -1) {
+            options.push(request.mediatype);
+        }
+        cordova.exec(successCallback, errorCallback, "Coap", "post", options);
     },
-    put: function (name, options, successCallback, errorCallback) {
+    put: function (request, successCallback, errorCallback) {
         if (typeof successCallback !== "function") {
-            console.log("put first parameter must be a function to handle responses.");
+            console.log("put second parameter must be a function to handle responses.");
             return;
         }
-            cordova.exec(successCallback, errorCallback, "Coap", "put", [name, options]);
+        var options = [];
+        if (request === null || typeof request !== "object") {
+            console.log("Coap: Invalid parameter. request not an object.");
+            return;
+        }
+        if (request.URI !== undefined && request.URI !== "") {
+            options.push(request.URI);
+        }
+        else {
+            console.log("Coap: No URI given.");
+            return false;
+        }
+        if (request.payload !== undefined) {
+            options.push(request.payload);
+        }
+        else {
+            console.log("Coap: No payload given.");
+            return false;
+        }
+        if (request.mediatype !== undefined && request.mediatype !== -1) {
+            options.push(request.mediatype);
+        }
+        cordova.exec(successCallback, errorCallback, "Coap", "put", options);
     },
     test: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Coap", "test");

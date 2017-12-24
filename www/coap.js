70104dc74eaad7bb2f1b38b64cd1cb03cead58ea
giacomo <giacomo.tanganelli@for.unipi.it>
2017-12-24 11:35:27 +0100

    js

--- www/coap.js
+++ www/coap.js
@@ -1,16 +1,30 @@
 /*global cordova, module*/
 
-module.exports = {
+var Coap = {
     get: function (name, successCallback, errorCallback) {
+        if (typeof successCallback !== "function") {
+            console.log("get first parameter must be a function to handle responses.");
+            return;
+        }
         cordova.exec(successCallback, errorCallback, "Coap", "get", [name]);
     },
     post: function (name, options, successCallback, errorCallback) {
+        if (typeof successCallback !== "function") {
+            console.log("post first parameter must be a function to handle responses.");
+            return;
+        }
         cordova.exec(successCallback, errorCallback, "Coap", "post", [name, options]);
     },
     put: function (name, options, successCallback, errorCallback) {
+        if (typeof successCallback !== "function") {
+            console.log("put first parameter must be a function to handle responses.");
+            return;
+        }
             cordova.exec(successCallback, errorCallback, "Coap", "put", [name, options]);
     },
     test: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Coap", "test");
     }
 };
+
+module.exports = Coap;

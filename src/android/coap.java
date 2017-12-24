4d350bcdfb959530e6abcbaf0a8349ef0a1dae62
Giacomo Tanganelli <jacko@mbp-di-giacomo.homenet.telecomitalia.it>
2017-12-24 11:17:22 +0100

    put

--- src/android/coap.java
+++ src/android/coap.java
@@ -3,6 +3,7 @@ package com.phodal.plugin;
 import org.apache.cordova.*;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -20,9 +21,19 @@ public class Coap extends CordovaPlugin {
             Log.d("Coap ", "\n test for get");
 
             try {
+                String uri_s = data.getString(0);
+                int mediatype = -1;
+                if(!data.isNull(1))
+                    mediatype = data.getInt(1);
+                if(!MediaTypeRegistry.getAllMediaTypes().contains(mediatype))
+                    mediatype = -1;
                 URI uri = new URI(data.getString(0));
                 CoapClient mCoapClient = new CoapClient(uri);
-                CoapResponse response = mCoapClient.get();
+                CoapResponse response = null;
+                if(mediatype!=-1)
+                    response = mCoapClient.get(mediatype);
+                else
+                    response = mCoapClient.get();
                 callbackContext.success(response.getResponseText());
                 return true;
             } catch (URISyntaxException e) {
@@ -33,15 +44,26 @@ public class Coap extends CordovaPlugin {
         } else if(action.equals("post")) {
             Log.d("Coap ", "\n test for post");
 
-            try {
-                URI uri = new URI(data.getString(0));
+            try{
+                String uri_s = data.getString(0);
+                String payload_s = data.getString(1);
+                int mediatype = MediaTypeRegistry.TEXT_PLAIN;
+                if(!data.isNull(2))
+                    mediatype = data.getInt(2);
+                if(!MediaTypeRegistry.getAllMediaTypes().contains(mediatype))
+                    mediatype = MediaTypeRegistry.TEXT_PLAIN;
+                URI uri = new URI(uri_s);
                 CoapClient mCoapClient = new CoapClient(uri);
-                CoapResponse response = mCoapClient.post(data.getString(1), 0);
+                CoapResponse response = mCoapClient.post(payload_s, mediatype);
                 callbackContext.success(response.getResponseText());
                 return true;
             } catch (URISyntaxException e) {
                 Log.e("Coap", "URISyntaxException");
                 callbackContext.error("URISyntaxException");
+                return false;
+            }catch (JSONException e) {
+                Log.e("Coap", "JSONException");
+                callbackContext.error("JSONException");
                 return false;
             } catch (Exception e) {
                 Log.e("Coap", "Exception");
@@ -50,16 +72,26 @@ public class Coap extends CordovaPlugin {
             }
         } else if(action.equals("put")) {
             Log.d("Coap ", "\n test for put");
-
-            try {
-                URI uri = new URI(data.getString(0));
+            try{
+                String uri_s = data.getString(0);
+                String payload_s = data.getString(1);
+                int mediatype = MediaTypeRegistry.TEXT_PLAIN;
+                if(!data.isNull(2))
+                    mediatype = data.getInt(2);
+                if(!MediaTypeRegistry.getAllMediaTypes().contains(mediatype))
+                    mediatype = MediaTypeRegistry.TEXT_PLAIN;
+                URI uri = new URI(uri_s);
                 CoapClient mCoapClient = new CoapClient(uri);
-                CoapResponse response = mCoapClient.put(data.getString(1), 0);
+                CoapResponse response = mCoapClient.put(payload_s, mediatype);
                 callbackContext.success(response.getResponseText());
                 return true;
             } catch (URISyntaxException e) {
                 Log.e("Coap", "URISyntaxException");
                 callbackContext.error("URISyntaxException");
+                return false;
+            }catch (JSONException e) {
+                Log.e("Coap", "JSONException");
+                callbackContext.error("JSONException");
                 return false;
             } catch (Exception e) {
                 Log.e("Coap", "Exception");
